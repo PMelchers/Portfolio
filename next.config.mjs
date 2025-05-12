e97bf2b894a11873c971d2c4d1769d4b9d3eb454
Pim Melchers <1210025@student.roc-nijmegen.nl>
2025-05-12 11:36:45 +0200

    Kansloos

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true,
   images: {
     domains: ['placeholder.com'],
     unoptimized: true,
