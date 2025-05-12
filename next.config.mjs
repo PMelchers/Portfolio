4c65a868b1485b5c2f965eddafc42e434321a986
Pim Melchers <1210025@student.roc-nijmegen.nl>
2025-05-12 11:32:44 +0200

    run build

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: false,
+  trailingSlash: true,
   images: {
     domains: ['placeholder.com'],
     unoptimized: true,
